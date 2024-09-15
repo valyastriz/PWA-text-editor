70f4419cc4b99e86cda3e273d4656e553dc015d4
valyastriz <valyastriz@gmail.com>
2024-09-15 14:38:06 -0400

    completed src-sw.js fkile

--- client/src-sw.js
+++ client/src-sw.js
@@ -5,8 +5,10 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// Precahses Webpack- genreted files
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Cache for page navigation (HTML requests)
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
   plugins: [
@@ -14,17 +16,50 @@ const pageCache = new CacheFirst({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Caches for 30 days
     }),
   ],
 });
 
+// Warm up the cach with these URLs
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
+// Cache navigation requests (like when a user clicks links)
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // Cache JS, CSS, and Web worker requests with a StaleWhile Revalidate strategy 
+  ({ request }) =>
+    request.destination === 'script' ||
+    request.destination === 'style' ||
+    request.destination === 'worker',
+  new StaleWhileRevlidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// Cache images with CacheFirst, which will store them for 30 days
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60, // Max number of images to cache
+        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 days
+      }),
+    ],
+  })
+);
