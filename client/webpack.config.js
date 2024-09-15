27628ad2f4faf55237055c6825eaff0214ae0c76
valyastriz <valyastriz@gmail.com>
2024-09-15 14:41:27 -0400

    updated minor changes to wepback.config file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,20 +3,18 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    devtool: 'source-map',  // Add source maps for easier debugging
     plugins: [
       // HTML Plugin to inject the bundles into the HTML file
       new HtmlWebpackPlugin({
@@ -30,8 +28,8 @@ module.exports = () => {
         short_name: 'JATE',
         description: 'A Progressive Web App text editor',
         background_color: '#ffffff',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         fingerprints: false,
         inject: true,
         icons: [
